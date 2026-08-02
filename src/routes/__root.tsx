6146153d0be5951411e9d03fdd4b6c55321c5b66
CrumpetCoding <seannrowe@gmail.com>
2026-08-02 18:15:08 +0100

    Re-aligned menu items

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,8 +5,8 @@ import { AppBar, Toolbar, Typography, IconButton, Icon } from '@mui/material'
 const RootLayout = () => (
   <>
     <div id="menu">
-      <AppBar position='static' sx={{ padding: '10px', backgroundColor: '#78A1BB' }}>
-        <Toolbar sx={{ display: 'flex', alignItems: 'center', gap: '12px' }}>
+      <AppBar position='static' sx={{ backgroundColor: '#78A1BB' }}>
+        <Toolbar sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '12px' }}>
           <Link to="/" className="[&.active]:font-bold">
             <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: 'white', '&:hover': { color: '#283044' } }}>
               Home
