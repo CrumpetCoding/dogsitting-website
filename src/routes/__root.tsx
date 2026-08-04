8a60c70e6f9b1cb011b99cdb281209562f719fde
CrumpetCoding <seannrowe@gmail.com>
2026-08-04 21:01:17 +0100

    Added buttons to toolbar

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,27 +1,33 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
-import { AppBar, Toolbar, Typography, IconButton, Icon } from '@mui/material'
+import { AppBar, Toolbar, Typography, Button } from '@mui/material'
 
 const RootLayout = () => (
   <>
     <div id="menu">
-      <AppBar position='static' sx={{ backgroundColor: '#78A1BB' }}>
+      <AppBar position='static'>
         <Toolbar sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '12px' }}>
-          <Link to="/" className="[&.active]:font-bold">
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: 'white', '&:hover': { color: '#283044' } }}>
-              Home
-            </Typography>
-          </Link>{' '}
-          <Link to="/about" className="[&.active]:font-bold">
-            <Typography variant='h6' component="div" sx={{ flexGrow: 1, color: 'white', '&:hover': { color: '#283044' } }}>
-              About
-            </Typography>
-          </Link>{' '}
-          <Link to="/contact" className="[&.active]:font-bold">
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: 'white', '&:hover': { color: '#283044' } }}>
-              Contact
-            </Typography>
-          </Link>
+          <Button variant='text'>
+            <Link to="/" className="[&.active]:font-bold">
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: 'white', '&:hover': { color: '#283044' } }}>
+                Home
+              </Typography>
+            </Link>{' '}
+          </Button>
+          <Button variant='text'>
+            <Link to="/about" className="[&.active]:font-bold">
+              <Typography variant='h6' component="div" sx={{ flexGrow: 1, color: 'white', '&:hover': { color: '#283044' } }}>
+                About
+              </Typography>
+            </Link>{' '}
+          </Button>
+          <Button variant='text'>
+            <Link to="/contact" className="[&.active]:font-bold">
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: 'white', '&:hover': { color: '#283044' } }}>
+                Contact
+              </Typography>
+            </Link>
+          </Button>
         </Toolbar>
       </AppBar>
     </div>
