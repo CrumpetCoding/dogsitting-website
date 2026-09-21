0598d12b93095ee02ae6e0545d0eea72661e9b22
CrumpetCoding <seannrowe@gmail.com>
2026-09-21 15:33:35 +0100

    Attempted email validation - Didn't work

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,4 +1,7 @@
 import { TextField, Button, Container, Paper } from "@mui/material";
+import { useState } from "react";
+
+
 
 export default function ContactForm() {
     return (
@@ -19,7 +22,9 @@ export default function ContactForm() {
             >
                 <TextField id="first-name" label="First Name" variant="outlined" sx={{ gridArea: 1 / 1 / 2 / 2 }} />
                 <TextField id="surname" label="Last Name" variant="outlined" sx={{ gridArea: 1 / 2 / 2 / 3 }} />
-                <TextField id="email" label="Email" variant="outlined" sx={{ gridArea: 2 / 1 / 3 / 3, gridColumn: '1 / span 2' }} />
+                <TextField id="email" label="Email" variant="outlined" sx={{ gridArea: 2 / 1 / 3 / 3, gridColumn: '1 / span 2' }}
+                    type="email"
+                />
                 <TextField id="message" label="Message" variant="outlined" sx={{ gridArea: 3 / 1 / 4 / 3, gridColumn: '1 / span 2', gridRow: ' 3 / span 2', textWrap: 'wrap' }} />
                 <Button id="submit" variant="contained" color="primary">Submit</Button>
             </Paper>
