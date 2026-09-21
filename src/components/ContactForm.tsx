04505a0940a3438c01a184fce5794637b255530e
CrumpetCoding <seannrowe@gmail.com>
2026-09-21 15:43:20 +0100

    Fixed issues with message box size

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -7,6 +7,7 @@ export default function ContactForm() {
     return (
         <Container
             maxWidth='xs'
+            sx={{ maxHeight: 'xs' }}
         >
             <Paper
                 elevation={10}
@@ -15,9 +16,8 @@ export default function ContactForm() {
                     padding: 2,
                     display: 'grid',
                     gridTemplateColumns: 'repeat(2, 1fr)',
-                    gridTemplateRows: 'repeat(3, 1fr)',
-                    gridColumnGap: '8px',
-                    gridRowGap: '8px'
+                    gridTemplateRows: 'repeat(3, 70px)',
+                    gap: '8px'
                 }}
             >
                 <TextField id="first-name" label="First Name" variant="outlined" sx={{ gridArea: 1 / 1 / 2 / 2 }} />
@@ -25,7 +25,7 @@ export default function ContactForm() {
                 <TextField id="email" label="Email" variant="outlined" sx={{ gridArea: 2 / 1 / 3 / 3, gridColumn: '1 / span 2' }}
                     type="email"
                 />
-                <TextField id="message" label="Message" variant="outlined" sx={{ gridArea: 3 / 1 / 4 / 3, gridColumn: '1 / span 2', gridRow: ' 3 / span 2', textWrap: 'wrap' }} />
+                <TextField id="message" label="Message" variant="outlined" multiline rows={4} sx={{ gridArea: 3 / 1 / 4 / 3, gridColumn: '1 / span 2', gridRow: ' 3 / span 2', textWrap: 'wrap' }} />
                 <Button id="submit" variant="contained" color="primary">Submit</Button>
             </Paper>
         </Container >
